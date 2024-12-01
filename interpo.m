47a0906b7c865a9a09e66a6499dbd6ebf445ad0c
RenderHoward <David.Howard@Ultrapoise.com>
2024-12-01 18:54:41 -0500

    Using Delaunay triangulation to interpolate control points.

--- interpo.m
+++ interpo.m
@@ -1,1 +1,6 @@
 [lat,lon,value] = textread ('TiePoints.dat','%f %f %f','headerlines',1);
+yi = linspace(8.653061, 17.22449, 70).';
+xi = linspace (121, 131, 70).';
+[xa, ya] = meshgrid(xi, yi);
+zi = griddata( lon, lat, value, ya, xa );
+mesh( ya, xa, zi);
